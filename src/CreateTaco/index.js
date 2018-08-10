3462c5d167ed4479e128726072be98c938d50d1f
Ron Clark <ronaldtclark@gmail.com>
2018-08-10 11:22:06 -0500

    fixed post

--- src/CreateTaco/index.js
+++ src/CreateTaco/index.js
@@ -9,28 +9,28 @@ class CreateTaco extends Component {
       comments: ['']
     }
   }
+  
   updateTaco = (e) => {
     this.setState({[e.currentTarget.name]: e.currentTarget.value});
   }
 
   
-
   render(){
     return(
       <form onSubmit={this.props.addTaco.bind(this, this.state)}>
         <span>
           <label>
             Taco:
-            <input class="create" type="text" name="name" onChange={this.updateTaco}/>
+            <input type="text" name="name" onChange={this.updateTaco}/>
           </label>
         </span>
         <span>
           <label>
             Restaurant:
-            <input class="create" type="text" name="restaurant" onChange={this.updateTaco}/>
+            <input type="text" name="restaurant" onChange={this.updateTaco}/>
           </label>
-          <input type='Submit'/>
         </span>
+          <input type='Submit'/>
       </form>
       
     )
