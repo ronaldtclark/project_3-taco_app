726d0b4955a28bbdec7e88493ae57f81f919acae
Ron Clark <ronaldtclark@gmail.com>
2018-08-10 10:01:24 -0500

    presentation readyish

--- taco-app-react/src/CreateTaco/index.js
+++ taco-app-react/src/CreateTaco/index.js
@@ -14,20 +14,25 @@ class CreateTaco extends Component {
   }
 
   
-  
+
   render(){
     return(
       <form onSubmit={this.props.addTaco.bind(this, this.state)}>
-        <label>
-          Taco:
-          <input type="text" name="name" onChange={this.updateTaco}/>
-        </label>
-        <label>
-          Restaurant:
-          <input type="text" name="restaurant" onChange={this.updateTaco}/>
-        </label>
-        <input type='Submit'/>
+        <span>
+          <label>
+            Taco:
+            <input class="create" type="text" name="name" onChange={this.updateTaco}/>
+          </label>
+        </span>
+        <span>
+          <label>
+            Restaurant:
+            <input class="create" type="text" name="restaurant" onChange={this.updateTaco}/>
+          </label>
+          <input type='Submit'/>
+        </span>
       </form>
+      
     )
   }
 }
